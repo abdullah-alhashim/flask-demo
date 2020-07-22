a89f68eb0e2c3dd774f111e376f4c4ade83d1608
Abdullah Alhashim <a.alhashim12@gmail.com>
2020-07-22 21:53:47 +0300

    Add homepage

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/hello")
+@app.route("/")
 def index():
+    return """
+    <form method='post' action='http://localhost:5000/hello'>
+        <input id='name' type='text' placeholder='name' name='name'>
+        <input type='submit'>
+    </form>
+    """
+
+
+@app.route("/hello")
+def hello():
     return "Hello"
+
+
+@app.route("/hello", methods=["post"])
+def hello_name():
+    name = request.form["name"]
+    return f"Hello {name}"
 
 
 if __name__ == '__main__':
